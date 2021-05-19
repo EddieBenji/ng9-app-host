2c2e1156b1edec8f598777467891462e3c4c2fed
Eduardo Canche Vazquez <eduardo.942712@gmail.com>
2021-05-19 17:10:18 -0500

    Flow for 'editing' an item with vanilla HTML/CSS/JS, added

--- src/app/utils/services/popup.service.ts
+++ src/app/utils/services/popup.service.ts
@@ -22,6 +22,9 @@ export class PopupService {
         }
         // Create element
         const popupEl: NgElement & WithProperties<any> = document.createElement(formCustomTag) as any;
+        // Set the element to edit (if any).
+        popupEl.itemToEdit = itemToEdit;
+
         const formBody = document.getElementById(bodyContainerId);
         // Listen to the close event
         popupEl.addEventListener('saveForm', (info: HTMLElementEventMap | any) => {
@@ -62,8 +65,6 @@ export class PopupService {
 
         // Set the id
         popupEl.id = formCustomTag;
-        // Set the element to edit (if any).
-        popupEl.itemToEdit = itemToEdit;
 
         // Add to the DOM
         formBody.appendChild(popupEl);
@@ -72,33 +73,4 @@ export class PopupService {
     get onFormClosed() {
         return this.formClosed$.asObservable();
     }
-
-    // This uses the new custom-element method to add the popup to the DOM.
-    // showAsElement(message: string) {
-    //     if (!message) {
-    //         console.log('Please, add something in the text input');
-    //         return;
-    //     }
-    //     const popupElRendered = document.getElementById('popup-element') as any;
-    //     if (popupElRendered) {
-    //         popupElRendered.message = message;
-    //         return;
-    //     }
-    //     // Create element
-    //     const popupEl: NgElement & WithProperties<any> = document.createElement('popup-element') as any;
-    //
-    //     // Listen to the close event
-    //     popupEl.addEventListener('closed', (info: HTMLElementEventMap | any) => {
-    //         console.log('info ->', info);
-    //         console.log('info ->', info.detail.data);
-    //         document.body.removeChild(popupEl);
-    //     });
-    //
-    //     // Set the message
-    //     popupEl.message = message;
-    //     popupEl.id = 'popup-element';
-    //
-    //     // Add to the DOM
-    //     document.body.appendChild(popupEl);
-    // }
 }
